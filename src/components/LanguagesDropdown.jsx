c2232b4f1228d3adad1c12c0930497744ed773a5
Karnik Kanojia <72961346+karnikkanojia@users.noreply.github.com>
2022-05-31 03:11:35 +0530

    Dropdown component for languages and themes done

--- src/components/LanguagesDropdown.jsx
+++ src/components/LanguagesDropdown.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import Select from 'react-select';
 import { languageOptions } from '../constants/languageOptions';
-import { customStyles } from "../constants/customStyles";
+import { customStyles } from '../constants/customStyles';
 
-const LanguagesDropdown = () => {
+const LanguagesDropdown = ({ onSelectChange }) => {
   return (
     <div>
       <Select
         placeholder="Filter by Category"
         options={languageOptions}
-
+        styles={customStyles}
+        defaultValue={languageOptions[0]}
+        onChange={(selectedOption) => onSelectChange(selectedOption)}
       />
     </div>
-  )
-}
+  );
+};
 
-export default LanguagesDropdown
+export default LanguagesDropdown;
