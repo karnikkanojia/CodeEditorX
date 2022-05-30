a2d403a6630a486faf512999642361c76947678d
Karnik Kanojia <72961346+karnikkanojia@users.noreply.github.com>
2022-05-31 02:24:25 +0530

    Intialised repository and added constant language options. Added util functions. Basic code editor component ready

---
+++ src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import CodeEditorWindow from './index';
+import axios from 'axios';
+import { classnames } from '../utils/general';
+import { languageOptions } from '../constants/languageOptions'
+
+const Landing = () => {
+  return (
+    <div>Landing</div>
+  )
+}
+
+export default Landing
---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as CodeEditorWindow } from './CodeEditorWindow';
+export { default as Landing } from './Landing';
