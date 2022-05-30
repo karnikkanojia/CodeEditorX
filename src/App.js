8b2be8dc431cabe0cb985e76c4a10b4761046d7c
Karnik Kanojia <72961346+karnikkanojia@users.noreply.github.com>
2022-05-31 03:05:06 +0530

    Main Landing component ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Landing } from './components'
 
 const App = () => {
   return (
     <div>
-        Hello World
+        <Landing />
     </div>
   )
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { classnames } from "../utils/general";
 import { languageOptions } from "../constants/languageOptions";
 
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import { defineTheme } from "../lib/defineTheme";
@@ -34,7 +34,130 @@ const Landing = () => {
   const enterPress = useKeyPress("Enter");
   const ctrlPress = useKeyPress("Control");
 
-  return <div>Landing</div>;
+  const onSelectChange = (sl) => {
+    console.log("Selected Option...", sl);
+    setLanguage(sl);
+  }
+
+  useEffect(() => {
+    if(enterPress && ctrlPress){
+      console.log("enterPress", enterPress);
+      console.log("ctrlPress", ctrlPress);
+      handleCompile();
+    }
+  }, [ctrlPress, enterPress]);
+
+  const onChange = (action, data) => {
+    switch (action){
+      case "code": {
+        setCode(data);
+        break;
+      }
+      default: {
+        console.warn("Case not handled!", action, data);
+      }
+    }
+  };
+
+  const handleCompile = () => {
+
+  };
+
+  const checkStatus = async (token) => {
+
+  };
+
+  const handleThemeChange = (th) => {
+
+  }
+
+  useEffect(() => {
+    defineTheme("oceanic-next").then(() => {
+      setTheme({ value: "oceanic-next", label: "Oceanic Next" });
+    });
+  }, []);
+
+  const showSuccessToast = (msg) => {
+    toast.success(msg || "Compiled Successfully!", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined
+    });
+  };
+
+  const showErrorToast = (msg) => {
+    toast.error(msg || "Something went wrong! Please try again.", {
+      position: "top-right",
+      autoClose: 1000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+
+      <div className="h-4 w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"></div>
+      <div className="flex flex-row">
+        <div className="px-4 py-2">
+          <LanguagesDropdown onSelectChange={onSelectChange} />
+        </div>
+        <div className="px-4 py-2">
+          <LanguagesDropdown onSelectChange={onSelectChange} />
+        </div>
+      </div>
+      <div className="flex flex-row space-x-4 items-start px-4 py-4">
+        <div className="flex flex-col w-full h-full justify-start items-end">
+          <CodeEditorWindow
+            code={code}
+            onChange={onChange}
+            language={language?.value}
+            theme={theme.value}
+          />
+        </div>
+
+        <div className="right-container flex flex-shrink-0 w-[30%] flex-col">
+          <OutputWindow outputDetails={outputDetails} />
+          <div className="flex flex-col items-end">
+            <CustomInput
+              customInput={customInput}
+              setCustomInput={setCustomInput}
+            />
+            <button
+              onClick={handleCompile}
+              disabled={!code}
+              className={classnames(
+                "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
+                !code? "opacity-50":""
+              )}
+            >
+              {processing ? "Processing..." : "Compile and Execute"}
+            </button>
+          </div>
+                { outputDetails && <OutputDetails outputDetails={outputDetails} /> }
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
 };
 
 export default Landing;
